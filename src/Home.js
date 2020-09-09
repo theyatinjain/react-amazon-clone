3492bc0146b943391d7aef45d665fa4430383cdd
Yatin Jain <rokstaryatin2012@gmail.com>
2020-09-09 14:21:17 +0530

    Navigation Bar static completed

---
+++ src/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__container">
+                <img className="home__image" src="https://images-eu.ssl-images-amazon.com/images/G/31/AmazonVideo/2020/X-site/Multititle/Aug/1500x600_Hero-Tall_np._CB404803728_.jpg" alt="Prime Video Poster" />
+                <div className="home__row">
+                    {/* Product component */}
+                    {/* Product component */}
+                </div>
+                <div className="home__row">
+                    {/* Product component */}
+                    {/* Product component */}
+                    {/* Product component */}
+                </div>
+                <div className="home__row">
+                    {/* Product component */}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Home
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAYgqXTrX1UjYc3SxZL207pD0_-hmnTE4k",
+    authDomain: "clone-9686d.firebaseapp.com",
+    databaseURL: "https://clone-9686d.firebaseio.com",
+    projectId: "clone-9686d",
+    storageBucket: "clone-9686d.appspot.com",
+    messagingSenderId: "262268663150",
+    appId: "1:262268663150:web:e2a16a773f31adbca5bd7f",
+    measurementId: "G-P7GDQW7C8X"
+  };
